aba0fb8b983d44ca82d389113ed773c30bef4ee9
Doris Lee <dorislee@berkeley.edu>
2016-07-29 18:41:32 -0700

    updated colorbar

--- scripts/makeMHDMovie.py
+++ scripts/makeMHDMovie.py
@@ -4,8 +4,8 @@ START= 0
 END = 198
 INTERVAL = 1
 import os
-os.chdir("/global/homes/d/dorislee/proj/dlee/FLASH4.3/SimDataMHDSphere/")
+os.chdir("/global/homes/d/dorislee/edscr/astroSim-data/SimDataMHDSphere/")
 for i in np.arange(START,END,INTERVAL):
     print "Working on t =", i
-    plot_var(i,cut="y",physical_quantity="density",magnetic=True,velocity=True,scale=True,save_path=SAVE_LOC)    
+    plot_var(i,cut="y",physical_quantity="density",zmin=5e-22,zmax=1e-17,magnetic=True,velocity=True,scale=True,save_path=SAVE_LOC)    
 os.system("convert -delay 0.1 {}* mhd_sphere_density.gif".format(SAVE_LOC))
